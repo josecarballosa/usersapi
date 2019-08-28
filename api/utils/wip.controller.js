75de39b23fb66d7799bd4d50dd55b5b0cb0efa38
Jose Carballosa <jose.carballosa@gmail.com>
2019-08-28 16:39:31 +0200

    changed status code for not-implemented to 501

--- api/utils/wip.controller.js
+++ api/utils/wip.controller.js
@@ -2,11 +2,11 @@ module.exports = {
 
 	handleParam: async (req, res, next, param) => {
 		// TODO: add debug log here with route details
-		res.status(500).json({ errors: {"method": "not implemented"} });
+		res.status(501).json({ errors: {"method": "not implemented"} });
 	},
 
 	handleRequest: async (req, res, next) => {
 		// TODO: add debug log here with route details
-	  res.status(500).json({ errors: {"method": "not implemented"} });
+	  res.status(501).json({ errors: {"method": "not implemented"} });
 	}
 }
