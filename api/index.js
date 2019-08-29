743760989f4bc155f2848354e0ca16136bc293fc
Jose Carballosa <jose.carballosa@gmail.com>
2019-08-29 13:21:05 +0200

    added database connection error handling

--- api/index.js
+++ api/index.js
@@ -6,10 +6,19 @@ const mongoose = require('mongoose');
 const { env, mongoDbUri } = require('./config');
 const router = require('./router');
 
-mongoose.connect(mongoDbUri, {
-	useNewUrlParser: true,
-	useCreateIndex: (env !== 'production'),
-});
+// TODO: Is a good practice to keep the DB connection open?
+
+(async () => {
+	try {
+		await mongoose.connect(mongoDbUri, {
+			useNewUrlParser: true,
+			useCreateIndex: (env !== 'production'),
+		});
+	} catch (error) {
+		console.log('Error: Failed to connect to the database');
+		process.exit(1);
+	}
+})();
 
 const api = express();
 
@@ -32,15 +41,15 @@ api.use((err, req, res, next) => {
   if (err.type === 'entity.parse.failed') {
     return res.status(400).json({ errors: {"body": "cannot be parsed"} });
 	 }
-  next();
+  next(err);
 });
 
 // Handle "express-jwt" errors
 api.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
-    return res.status(401).json({ errors: {"token": "is invalid"} });
+    return res.status(401).json({ errors: {"auth token": "is invalid"} });
 	 }
-  next();
+  next(err);
 });
 
 // Handle unexpected errors
